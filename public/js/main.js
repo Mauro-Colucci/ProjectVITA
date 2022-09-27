2db36ed7b7a8d61f58a30bc6ce0d87072c075fd3
Mauro Colucci <colucci.mauro@gmail.com>
2022-09-27 18:53:31 -0300

    fixed sidebar issue due to char.js blocking js due to not being rendered in all pages

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 let btn = document.querySelector('.profile')
 const wrapper = document.querySelector('.wrapper')
 let sideNav = localStorage.getItem("sideNav");
+const ctx = document.getElementById('myChart')//.getContext('2d');
+const line = document.getElementById('lineChart')//.getContext('2d');
 
 
 const activeSidenav = () => {
@@ -22,3 +24,70 @@ btn.addEventListener('click', () => {
     sideNav = localStorage.getItem("sideNav")
     sideNav? inactiveSidenav():activeSidenav()
 });
+
+//this prevents the instantiation of chart in pages that do not have the canvas with the id listed above
+if (ctx !== null && line !== null){
+    ctx.getContext('2d');
+    line.getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true
+        }
+    });
+
+    const lineChart = new Chart(line, {
+        type: 'line',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true
+        }
+    });
+}
