7a0c7a1fd4a069ccd5d8d9282d85a8acdf0bcd6e
Mauro Colucci <colucci.mauro@gmail.com>
2022-10-11 16:59:17 -0300

    fixed project links for static forms

--- controllers/charts.js
+++ controllers/charts.js
@@ -7,19 +7,32 @@ const User = require("../models/User");
 module.exports = {
   //I'll use user ids in params.id
   getChartData: async (req, res) => {
-    let user, task, project, comment
+    let user, task, project, comment, closedTasks
     try {
       user = await User.find().populate('assignedTasks').lean();
       task = await Task.find({ status: {$ne: "closed"} }).lean();
+      closedTasks = await Task.find({ status: "closed"}).lean();
       project = await Project.find().populate("user tasks").lean();
       comment = await Comment.find().lean();
       if (req.params.id){
         user = await User.findById(req.params.id).lean();
-        task = await Task.find({$or: [ { createdBy: req.params.id }, { assignedTo: req.params.id }]}).lean();
+        task = await Task.find({
+          $or: [ 
+            { createdBy: req.params.id }, 
+            { assignedTo: req.params.id }
+          ]}).lean();
+        closedTasks = await Task.find({
+          $and: [{
+            $or: [
+              { createdBy: req.params.id }, 
+              { assignedTo: req.params.id }
+            ]}, 
+            {status: "closed"}
+          ]}).lean();
         project = await Project.find({user: req.params.id}).lean(); 
         comment = await Comment.find({createdBy: req.params.id}).lean();
       }
-      res.status(200).json({user, task, project, comment})
+      res.status(200).json({user, task, project, comment, closedTasks})
     } catch (err) {
       console.log(err);
     }
